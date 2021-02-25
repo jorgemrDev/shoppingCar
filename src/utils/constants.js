df22840035e27885672d8203bbcc03bffd2999ae
jorge <black.spade.666@gmail.com>
2021-02-25 14:44:18 -0500

    images base path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const urlApiProducts = "https://demo2562028.mockable.io/products";
-export const BASE_PATH = "http://localhost:3000";
+// export const BASE_PATH = "http://localhost:3000";
+export const BASE_PATH = "https://cranky-haibt-61710b.netlify.app/";
 export const STORAGE_PRODUCTS_CAR = "productCar";
